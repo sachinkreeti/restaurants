ecf0f5f80ba992522fc5401aa4deebee8780f1ac
sachinkreeti <sgupta@kreeti.com>
2023-11-22 16:49:48 +0530

    adds rack-mini-profiler gem

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,11 +3,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants or /restaurants.json
   def index
-    result = Benchmark.measure do
-      @restaurants = Restaurant.all
-    end
-
-    Rails.logger.info("Index Page Load Time: #{result.real} seconds")
+    @restaurants = Restaurant.all
   end
 
   # GET /restaurants/1 or /restaurants/1.json
